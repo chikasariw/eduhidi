6eb8a752a4644e711e171ac0a49eaa4d8f095d5c
chikasariw <chikasariw@gmail.com>
2025-11-23 01:13:21 +0700

    renamed

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ menuBtn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
     menu.classList.toggle('slide-down');
 });
-
-// Ambil semua checkbox
 const checkboxes = document.querySelectorAll('.todo-checkbox');
 
-// Load status dari localStorage saat halaman dibuka
 checkboxes.forEach(cb => {
     const saved = localStorage.getItem(cb.id);
     if (saved === 'true') {
         cb.checked = true;
         cb.nextElementSibling.classList.add('line-through', 'text-gray-400');
     }
-    // Event saat dicentang
     cb.addEventListener('change', () => {
         localStorage.setItem(cb.id, cb.checked);
         if (cb.checked) {
