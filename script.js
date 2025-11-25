711d255341c4384f82e806d6407765b6f7d2606d
chikasariw <chikasariw@gmail.com>
2025-11-25 19:05:51 +0700

    update navbar mobile, add materi, add aksi

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+document.addEventListener("DOMContentLoaded", () => {
+    
+ 
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-    menu.classList.toggle('slide-down');
-});
-const checkboxes = document.querySelectorAll('.todo-checkbox');
+    const checkboxes = document.querySelectorAll('.todo-checkbox');
 
-checkboxes.forEach(cb => {
-    const saved = localStorage.getItem(cb.id);
-    if (saved === 'true') {
-        cb.checked = true;
-        cb.nextElementSibling.classList.add('line-through', 'text-gray-400');
-    }
-    cb.addEventListener('change', () => {
-        localStorage.setItem(cb.id, cb.checked);
-        if (cb.checked) {
+    checkboxes.forEach(cb => {
+        const saved = localStorage.getItem(cb.id);
+
+        if (saved === 'true') {
+            cb.checked = true;
             cb.nextElementSibling.classList.add('line-through', 'text-gray-400');
-        } else {
-            cb.nextElementSibling.classList.remove('line-through', 'text-gray-400');
         }
+
+        cb.addEventListener('change', () => {
+            localStorage.setItem(cb.id, cb.checked);
+
+            if (cb.checked) {
+                cb.nextElementSibling.classList.add('line-through', 'text-gray-400');
+            } else {
+                cb.nextElementSibling.classList.remove('line-through', 'text-gray-400');
+            }
+        });
     });
 });
